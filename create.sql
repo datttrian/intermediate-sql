6b16836ba9f8feb98f356f9035e03838296e52e9
datttrian <datttrian@gmail.com>
2024-05-02 14:27:19 +0000

    Thu, May 2, 2024, 2:27 PM +00:00

--- create.sql
+++ create.sql
@@ -1,6 +1,40 @@
-CREATE TABLE People (
-    ID INT PRIMARY KEY,
-    Name VARCHAR(255),
-    Birthdate DATE,
-    AdditionalInfo VARCHAR(255)
+CREATE TABLE people (
+    id INT PRIMARY KEY,
+    name VARCHAR(255),
+    birthdate DATE,
+    deathdate DATE
+);
+
+CREATE TABLE films (
+    id INT PRIMARY KEY,
+    title VARCHAR(255),
+    release_year INT,
+    country VARCHAR(255),
+    duration INT,
+    language VARCHAR(255),
+    certification VARCHAR(255),
+    gross INT,
+    budget INT
+);
+
+CREATE TABLE roles (
+    id INT PRIMARY KEY,
+    title VARCHAR(255),
+    release_year INT,
+    country VARCHAR(255),
+    duration INT,
+    language VARCHAR(255),
+    certification VARCHAR(255),
+    gross INT,
+    budget INT
+);
+
+CREATE TABLE reviews (
+    id INT PRIMARY KEY,
+    film_id INT,
+    num_user INT,
+    num_critic INT,
+    imdb_score FLOAT,
+    num_votes INT,
+    facebook_likes INT
 );
